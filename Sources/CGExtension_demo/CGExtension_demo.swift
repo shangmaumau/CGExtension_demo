91e93b134f72b88edf9789eeeebd989c07ab0ff4
尚毛毛 <shangmaumau@live.com>
2023-08-11 16:59:02 +0800

    first commit

--- Sources/CGExtension_demo/CGExtension_demo.swift
+++ Sources/CGExtension_demo/CGExtension_demo.swift
@@ -1,2 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import CoreGraphics
+
+
+extension CGPoint {
+    public static func make(_ x: CGFloat, _ y: CGFloat) -> CGPoint {
+        return CGPoint(x: x, y: y)
+    }
+}
